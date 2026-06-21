958642d9d7ab46c49bab20a9ca8dd1caef912c97
Poojalakshmi <muthukumarappo@gmail.com>
2026-06-22 00:52:54 +0530

    Added text-to-speech feature

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 import streamlit as st
 from deep_translator import GoogleTranslator
+from gtts import gTTS
 
-st.title("Language Translator")
-st.write("Translate text instantly into multiple languages")
+st.title("🌍 AI Language Translator")
+
+st.write("Translate text instantly into multiple languages.")
 
 text = st.text_area("Enter Text")
 
 target = st.selectbox(
     "Translate To",
-    ["tamil", "hindi", "french", "english","telugu","malayalam","kannada","german","spanish","japanese","korean","chinese","viatnamese"]
+    [
+        "english",
+        "tamil",
+        "hindi",
+        "telugu",
+        "malayalam",
+        "kannada",
+        "french",
+        "german",
+        "spanish",
+        "japanese",
+        "korean",
+        "chinese"
+    ]
 )
 
 if st.button("Translate"):
 
     if text == "":
         st.warning("Please enter some text")
+
     else:
         translated = GoogleTranslator(
             source="auto",
@@ -22,4 +38,20 @@ if st.button("Translate"):
         ).translate(text)
 
         st.success(translated)
-    
+
+        st.subheader("📋 Copy Translation")
+        st.text_area(
+            "Translated Text",
+            translated,
+            height=100
+        )
+
+        st.subheader("🔊 Listen to Translation")
+
+        tts = gTTS(text=translated, lang="en")
+
+        tts.save("translation.mp3")
+
+        audio_file = open("translation.mp3", "rb")
+
+        st.audio(audio_file.read(), format="audio/mp3")
